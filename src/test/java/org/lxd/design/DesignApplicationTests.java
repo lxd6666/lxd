5e2b86f506e8b199f88d2b70972b2f8e2fffe089
lxd6666 <870723944@qq.com>
2020-07-07 10:37:42 +0800

    冲突测试

--- src/test/java/org/lxd/design/DesignApplicationTests.java
+++ src/test/java/org/lxd/design/DesignApplicationTests.java
@@ -2,11 +2,14 @@ package org.lxd.design;
 
 import com.alibaba.fastjson.JSON;
 import org.junit.jupiter.api.Test;
+import org.lxd.design.store.ICommodity;
+import org.lxd.design.store.StoreFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 class DesignApplicationTests {
@@ -56,6 +59,32 @@ class DesignApplicationTests {
         AwardRes awardRes03 = prizeController.awardToUser(req03);
         logger.info("请求参数：{}", JSON.toJSON(req03));
         logger.info("测试结果：{}", JSON.toJSON(awardRes03));
+
+    }
+    @Test
+    public void test_commodity() throws Exception {
+        StoreFactory storeFactory = new StoreFactory();
+
+        // 1. 优惠券
+        ICommodity commodityService_1 = storeFactory.getCommodityService(1);
+        commodityService_1.sendCommodity("10001", "EGM1023938910232121323432", "791098764902132", null);
+
+        // 2. 实物商品
+        ICommodity commodityService_2 = storeFactory.getCommodityService(2);
+        Map<String,String> extMap = new HashMap<String,String>();
+        extMap.put("consigneeUserName", "谢飞机");
+        extMap.put("consigneeUserPhone", "15200292123");
+        extMap.put("consigneeUserAddress", "吉林省.长春市.双阳区.XX街道.檀溪苑小区.#18-2109");
+
+        commodityService_2.sendCommodity("10001","9820198721311","1023000020112221113",new HashMap<String, String>() {{
+            put("consigneeUserName", "谢飞机");
+            put("consigneeUserPhone", "15200292123");
+            put("consigneeUserAddress", "吉林省.长春市.双阳区.XX街道.檀溪苑小区.#18-2109");
+        }});
+
+        // 3. 第三方兑换卡(爱奇艺)
+        ICommodity commodityService_3 = storeFactory.getCommodityService(3);
+        commodityService_3.sendCommodity("10001","AQY1xjkUodl8LO975GdfrYUio",null,null);
 
     }
 
